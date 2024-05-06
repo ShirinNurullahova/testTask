d6f19f827b0d32dc0a71ca994f7e296e4d73d7f5
ShirinNurullahova <sirin.nurullahova@mail.ru>
2024-05-06 12:28:35 +0400

    using StringBuilder

---
+++ src/Main10.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String oldStr = getWord(scanner);
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(oldStr);
+        String newStr = "";
+        changeStr(newStr, oldStr);
+
+    }
+    public static void changeStr(String newStr,String oldStr) {
+         for(int i=0; i<oldStr.length();i++){
+               newStr+= oldStr.charAt(oldStr.length()-1-i);
+         }
+        isPalendrome(newStr, oldStr);
+    }
+    public static boolean isPalendrome(String newStr, String oldStr) {
+        if(newStr.equals(oldStr)){
+            System.out.println("String palendromedir");
+            return true;
+        }else {
+            System.out.println("Palendrome deyil");
+            return false;
+        }
+    }
+    public static String getWord(Scanner sc){
+        System.out.print("Soz daxil et: ");
+        String oldStr = sc.nextLine();
+        return oldStr;
+    }
+}
---
+++ src/Main8.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String element = getString(sc);
+        enteredString(element);
+    }
+    public static void enteredString(String element){
+         StringBuilder stringBuilder = new StringBuilder();
+         stringBuilder.append(element);
+         String newText = "";
+        for(int i =0; i<stringBuilder.length();i++){
+           newText+= stringBuilder.charAt(stringBuilder.length()-i-1);
+        }
+        System.out.print( newText);
+    }
+    public static String getString(Scanner sc){
+        System.out.print("Soz daxil et: ");
+        String element = sc.nextLine();
+        return element;
+    }
+}
